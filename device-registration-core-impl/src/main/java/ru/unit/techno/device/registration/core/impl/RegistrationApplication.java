52adf20fe071001b4ceb451121e0fe06fdd550fb
Alexey Vasin <empty>
2021-07-07 20:24:35 +0300

    feature: init impl module

---
+++ device-registration-core-impl/src/main/java/ru/unit/techno/device/registration/core/impl/RegistrationApplication.java
@@ -0,0 +1,14 @@
+
+package ru.unit.techno.device.registration.core.impl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationApplication.class, args);
+    }
+
+}
