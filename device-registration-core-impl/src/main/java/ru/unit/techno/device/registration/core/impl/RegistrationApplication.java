7db81ea0209224e1591e41df310a44ad5a58c9da
Alexey Vasin <empty>
2021-07-15 20:41:50 +0300

    task: add eureka support

--- device-registration-core-impl/src/main/java/ru/unit/techno/device/registration/core/impl/RegistrationApplication.java
+++ device-registration-core-impl/src/main/java/ru/unit/techno/device/registration/core/impl/RegistrationApplication.java
@@ -3,8 +3,10 @@ package ru.unit.techno.device.registration.core.impl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RegistrationApplication {
 
     public static void main(String[] args) {
--- device-registration-core-impl/src/main/java/ru/unit/techno/device/registration/core/impl/resource/RegistrationController.java
+++ device-registration-core-impl/src/main/java/ru/unit/techno/device/registration/core/impl/resource/RegistrationController.java
@@ -2,6 +2,7 @@ package ru.unit.techno.device.registration.core.impl.resource;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +22,10 @@ public class RegistrationController {
     @ResponseStatus(HttpStatus.CREATED)
     public Long createUser(@RequestBody RegistrationDto registrationDto) {
         return registrationService.registerGroup(registrationDto);
+    }
+
+    @GetMapping("/test")
+    public String testController() {
+        return "success";
     }
 }
