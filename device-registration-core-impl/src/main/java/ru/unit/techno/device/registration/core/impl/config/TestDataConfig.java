e7d03b3d8a8b1cca00a50d5a86054f4a5118a4ef
Alexey Vasin <empty>
2021-07-21 21:45:30 +0300

    task: add test init data

---
+++ device-registration-core-impl/src/main/java/ru/unit/techno/device/registration/core/impl/config/TestDataConfig.java
@@ -0,0 +1,52 @@
+
+package ru.unit.techno.device.registration.core.impl.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.Transactional;
+import ru.unit.techno.device.registration.core.impl.entity.BarrierEntity;
+import ru.unit.techno.device.registration.core.impl.entity.GroupsEntity;
+import ru.unit.techno.device.registration.core.impl.entity.RfidDeviceEntity;
+import ru.unit.techno.device.registration.core.impl.entity.enums.DeviceType;
+import ru.unit.techno.device.registration.core.impl.repository.BarrierRepository;
+import ru.unit.techno.device.registration.core.impl.repository.GroupsRepository;
+import ru.unit.techno.device.registration.core.impl.repository.RfidDevicesRepository;
+
+/**
+ * TestDataConfig.
+ *
+ * @author Aleksey_Vasin
+ */
+//@Profile("local")
+@Configuration
+@RequiredArgsConstructor
+public class TestDataConfig {
+
+    private final GroupsRepository groupsRepository;
+    private final RfidDevicesRepository rfidDevicesRepository;
+    private final BarrierRepository barrierRepository;
+
+    @Bean
+    @Transactional
+    public void createTestGroup() {
+        var group = groupsRepository.save(
+                new GroupsEntity()
+                        .setAddress("test-address")
+                        .setGroupId(1L)
+
+        );
+        barrierRepository.save(new BarrierEntity()
+                .setGroup(group)
+                .setDeviceId(2345L)
+                .setType(DeviceType.ENTRY));
+
+        rfidDevicesRepository.save(
+                new RfidDeviceEntity()
+                        .setGroup(group)
+                        .setDeviceId(2345L)
+                        .setType(DeviceType.RFID)
+        );
+    }
+
+}
